32528f825be578ffe6455a35e24b3a53f3e636bf
LaVideos <lavideos1313@gmail.com>
2022-05-13 22:45:46 +0300

    aaaaaaaaaaaaaaa

--- js.js
+++ js.js
@@ -1,17 +1,71 @@
-let duplicate = string => {
-    let str = string.toLowerCase();
-    let arr = [];
+// let duplicate = string => {
+//     let str = string.toLowerCase();
+//     let arr = [];
+//
+//     for (let i = 0; i < str.length; i++) {
+//         let a = str.split(str[i]);
+//         if (a.length > 2) {
+//             arr.push(')');
+//         } else {
+//             arr.push('(');
+//         }
+//     }
+//     let b = arr.join('');
+//     console.log(b);
+// }
+//
+// duplicate('дарова lf');
 
-    for (let i = 0; i < str.length; i++) {
-        let a = str.split(str[i]);
-        if (a.length > 2) {
-            arr.push(')');
-        } else {
-            arr.push('(');
+
+
+let duplicate3 = string =>{
+    let a = string;
+    let h = string.split('');
+    for (let i = 0; i < string.length; i++) {
+        h.shift();
+        for (let j = 0; j < h.length; j++) {
+            if(h.includes(string[i])){
+                a = a.replaceAll(string[i],')');
+            }else {
+                a = a.replaceAll(string[i],'(');
+            }
         }
     }
-    let b = arr.join('');
-    console.log(b);
-}
 
-duplicate('дарова');
+    console.log(a);
+
+}
+duplicate3('heh hah');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let duplicate2 = string =>{
+//     let a = [];
+//     for (let i = 0; i < string.length; i++) {
+//         a.push(string.indexOf(string[i]));
+//     }
+//     console.log(a);
+//
+// }
+//
+// duplicate2('hello')
+
